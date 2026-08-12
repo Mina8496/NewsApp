1c02a3bf5afbb1de9f8ebf966f66b5dcfaaa5345
Mina Kamil <mena.m4077@gmail.com>
2026-08-12 18:33:39 +0300

    chore(feature): create file business_page and add files

---
+++ lib/feature/Business_page/presentation/pages/BusinessPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class Businesspage extends StatelessWidget {
+  const Businesspage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Business Page',
+        style: TextStyle(fontSize: 24),
+      )
+    );
+  }
+}
