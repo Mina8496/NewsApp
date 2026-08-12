765840ff380e73446e944cd23d957b4dc7e369ae
Mina Kamil <mena.m4077@gmail.com>
2026-08-12 18:34:02 +0300

    chore(feature): create file science_page and add files

---
+++ lib/feature/science_page/presentation/pages/sciencePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class Sciencepage extends StatelessWidget {
+  const Sciencepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Science Page',
+        style: TextStyle(fontSize: 24),
+      )
+    );
+  }
+}
