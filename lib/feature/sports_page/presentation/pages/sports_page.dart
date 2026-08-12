f7d620b41206cc16f53af7977926e64637631a70
Mina Kamil <mena.m4077@gmail.com>
2026-08-12 18:34:24 +0300

    chore(feature): create file sports_page and add files

---
+++ lib/feature/sports_page/presentation/pages/sports_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class SprtsPage extends StatelessWidget {
+  const SprtsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Sprts Page', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
