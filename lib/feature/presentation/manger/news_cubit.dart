adb1fa136c6dbc1c77e2531657afb4f4306ed693
Mina Kamil <mena.m4077@gmail.com>
2026-08-12 18:13:07 +0300

    feat(add Cubit): create pages cubit "news_stats", "news_cubit"

---
+++ lib/feature/presentation/manger/news_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/feature/presentation/manger/news_state.dart';
+
+class NewsCubit extends Cubit<NewsState> {
+  NewsCubit() : super(NewsInitialNewsState());
+
+  static NewsState get(context) => BlocProvider.of(context);
+}
---
+++ lib/feature/presentation/manger/news_state.dart
@@ -0,0 +1,5 @@
+abstract class NewsState {}
+
+class NewsInitialNewsState extends NewsState {}
+
+class NewsButtonNavBarState extends NewsState {}
