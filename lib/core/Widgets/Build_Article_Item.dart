ec1b3459cbd895155c70458c9f5887d46d5d3f89
Mina Kamil <mena.m4077@gmail.com>
2026-09-22 17:56:44 +0300

    feat(webview): add web view page and update dependencies

--- lib/core/Widgets/Build_Article_Item.dart
+++ lib/core/Widgets/Build_Article_Item.dart
@@ -1,52 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/core/routes/app_navigator.dart';
+import 'package:news_app/feature/web_view_page/presentation/page/web_view_page.dart';
 
 class BuildArticleItem extends StatelessWidget {
   final dynamic articles;
   final BuildContext context;
-  const BuildArticleItem({super.key, required this.articles, required this.context});
+  const BuildArticleItem({
+    super.key,
+    required this.articles,
+    required this.context,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20),
-      child: Row(
-        children: [
-          Container(
-            width: 120,
-            height: 120,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              image: DecorationImage(
-                image: NetworkImage('${articles['urlToImage']}'),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          SizedBox(width: 20),
-          Expanded(
-            child: Container(
+    return InkWell(
+      onTap: () {
+        AppNavigator.push(context, WebViewPage(url: articles['url']));
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Container(
+              width: 120,
               height: 120,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Text(
-                      '${articles['title']}',
-                      style: Theme.of(context).textTheme.bodySmall,
-                      maxLines: 4,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                  Text(
-                    '${articles['publishedAt']}',
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                ],
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: NetworkImage('${articles['urlToImage']}'),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-        ],
+            SizedBox(width: 20),
+            Expanded(
+              child: Container(
+                height: 120,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Text(
+                        '${articles['title']}',
+                        style: Theme.of(context).textTheme.bodySmall,
+                        maxLines: 4,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    Text(
+                      '${articles['publishedAt']}',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
